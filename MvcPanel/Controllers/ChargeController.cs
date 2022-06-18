3b7df2208bc29eff73e49c35695dd17e67c8cfe1
a.gaeini <a.gaeini@AGHIL-PC>
2022-06-18 17:12:27 +0430

    create payement page (not completed)

---
+++ MvcPanel/Controllers/ChargeController.cs
@@ -0,0 +1,49 @@
+﻿using DatabaseDomain.DTOs.Charge;
+using DatabaseDomain.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace MvcPanel.Controllers
+{
+    [Authorize]
+    public class ChargeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ChargeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShaparakPaymnet(string id)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(id);
+            var plainText = System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+
+            var payObject = JsonSerializer.Deserialize<Dictionary<string, object>>(plainText);
+
+            ViewBag.Username = payObject["Username"];
+            ViewBag.Amount = payObject["Amount"];
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ShaparakPaymnet(ShaparakPaymentDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            return View(model);
+        }
+
+    }
+}
--- MvcPanel/Controllers/PassengerController.cs
+++ MvcPanel/Controllers/PassengerController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MvcPanel.Controllers
 {
+    [Authorize]
     public class PassengerController : Controller
     {
         public IActionResult Index()
